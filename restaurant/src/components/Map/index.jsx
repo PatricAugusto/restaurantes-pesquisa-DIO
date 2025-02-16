bea21dc27865a791e0bd36066932173f394faf09
Patric Augusto <druidacanabico@gmail.com>
2025-02-16 20:07:26 -0300

    Estudo DIO

--- restaurant/src/components/Map/index.jsx
+++ restaurant/src/components/Map/index.jsx
@@ -49,4 +49,63 @@ export const MapContainer = (props) => {
     },
     [google, map, dispatch]
   );
-}
+
+  useEffect(() => {
+    if (query) {
+      searchByQuery(map, query);
+    }
+  }, [searchByQuery, query, map]);
+
+  useEffect(() => {
+    if (placeId) {
+      getDetails(placeId);
+    }
+  }, [placeId, getDetails]);
+
+  const searchNearby = (map, center) => {
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      location: center,
+      radius: '20000',
+      type: ['restaurant'],
+    };
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        dispatch(setRestaurants(results));
+      }
+    });
+  };
+
+  function onMapReady(_, map) {
+    setMap(map);
+    searchNearby(map, map.center);
+  }
+
+  return (
+    <Map
+      google={google}
+      centerAroundCurrentLocation
+      onReady={onMapReady}
+      onRecenter={onMapReady}
+      zoom={15}
+      {...props}>
+      {restaurants.map((restaurant) => (
+        <Marker
+          key={restaurant.place_id}
+          name={restaurant.name}
+          position={{
+            lat: restaurant.geometry.location.lat(),
+            lng: restaurant.geometry.location.lng(),
+          }}
+        />
+      ))}
+    </Map>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  language: 'pt-BR',
+})(MapContainer);
